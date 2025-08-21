8b09273189cf23f72fd01b7b7f3781a5d3b7171c
BERNARDO RABELO MUNDO <mundo.0957@aluno.pr.senac.br>
2025-08-21 11:53:02 -0300

    Criando a funcionalidade de deletar tarefa , criando novo arquivo delete.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <!-- FONT AWESOME -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
         integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+ 
 </head>
  
 <body>
@@ -25,80 +26,75 @@
                     <!-- Formulário -->
                     <div class="card card-body">
                         <form action="save.php" method="POST">
-                            <div class="form-group">
+                            <div class="form-geoup">
                                 <input type="text" name="title" class="form-control"
-                                    placeholder="Título da tarefa" autofocus>
+                                    placeholder="Titulo da tarefa" autofocus>
                             </div>
                             <div class="form-group">
                                 <textarea name="description" rows="2" class="form-control"
-                                    placeholder="Descrição da tarefa"></textarea>
+                                    placeholder="descricao da tarefa">
+                                </textarea>
                             </div>
                             <input type="submit" name="salvar" class="btn btn-success btn-block"
                                 value="Salvar tarefa">
                         </form>
                     </div>
                 </div>
-                <!-- /Formulário -->
-
+                <!-- Formulário -->
                 <!-- Tabela de tarefas -->
                 <div class="col-md-8">
                     <table class="table table-bordered">
                         <thead>
                             <tr>
-                                <th>ID</th>
-                                <th>Título</th>
-                                <th>Descrição</th>
-                                <th>Criado em</th>
+                                <th>Cód</th>
+                                <th>Titulo</th>
+                                <th>descrição</th>
+                                <th>Criado em </th>
                                 <th>Ação</th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php
                             require_once 'conn.php';
-                           
-                            $query = "SELECT id, title, description, created_at FROM crud_php";
-                            $result = $conn->query($query);
  
-                            if ($result && $result->num_rows > 0){
-                                while($row = $result->fetch_assoc()){
-                            ?>                
-                                  <tr>
-                                    <td><?php echo $row['id'];?></td>
-                                    <td><?php echo $row['title'];?></td>
-                                    <td><?php echo $row['description'];?></td>
-                                    <td><?php echo $row['created_at'];?></td>
-                                    <td>
-                                        <a href="#" class="btn btn-secondary">
-                                            <i class="fas fa-marker"></i>
-                                        </a>
-                                        <a href="#" class="btn btn-danger">
-                                            <i class="far fa-trash-alt"></i>
-                                        </a>
-                                    </td>
-                                  </tr>
-                            <?php
-                                }
-                            } else {
-                                echo "<tr><td colspan='5'>Nenhuma tarefa encontrada!</td></tr>";
-                            }
+                            $query = "SELECT id, title,description, created_at FROM crud_php";
+                            $result = $conn ->query($query);
+ 
+                            if($result -> num_rows > 0){
+                                while($row = $result ->fetch_assoc()){
+                                   
+                     
                             ?>
+                            <tr>
+                                <td><?php echo $row['id'];?></td>
+                                <td><?php echo $row['title'];?></td>
+                                <td><?php echo $row['description'];?></td>
+                                <td><?php echo $row['created_at'];?></td>
+                                <td><a href="#" class="btn btn-secondary">
+                                        <i class="fas fa-marker"></i>
+                                    </a>
+                                    <a href="delete.php?id=<?=$row['id']; ?>" class="btn btn-danger">
+                                        <i class="far fa-trash-alt"></i>
+                                    </a>
+                            </tr>
+                            <?php
+                               }
+                            } else{
+                                echo"<tr><td colspan ='5'> Nenhuma tarefa encontrada!</td></tr>";
+                            }
+                                $conn->close();
+                         ?>  
                         </tbody>
                     </table>
                 </div>
-                <!-- /Tabela de tarefas -->
+                <!-- /tabela de tarefas -->
             </div>
         </main>
     </div>
-
     <!-- BOOTSTRAP 4 SCRIPTS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
-        integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
-        integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
-        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 </body>
+ 
 </html>
